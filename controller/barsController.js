c26f5ff86ace481f383dbd1a8863ec1e69b0762f
Zedomas <Zedomas@gmail.com>
2020-07-08 10:57:16 -0500

    Added new routes

---
+++ controller/barsController.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bars = express.Router()
+const chicagoBars = require('../models/bars.js')
+const neighborhoods = ['Rogers Park', 'West Ridge', 'Uptown', 'Lincoln Square', 'North Center', 'Lake View', 'Lincoln Square', 'Near Noth Side', 'Edison Park', 'Norwood Park', 'Jefferson Park', 'Forest Glen', 'North Park', 'Albany Park', 'Portage Park', 'Irving Park', 'Dunning', 'Montclare', 'Belmont Cragin', 'Hermosa', 'Avondale', 'Logan Square', 'Humboldt Park', 'West Town', 'Austin', 'Near West Side', 'Lower West Side', 'Loop', 'Near South Side', 'Armour Square'];
+
+
+// Index route
+bars.get('/', (req, res) => {
+    chicagoBars.find({}, (errors, allBars) => {
+        res.render('index.ejs', {
+            Bars: allBars
+        })
+    })
+})
+
+// new Bar route
+bars.get('/new', (req, res) => {
+    chicagoBars.find({}, (errors, allBars) => {
+        res.render('newbar.ejs', {
+            Bars: allBars,
+            neighborhoods
+        })
+    })
+})
+
+bars.post('/', (req, res) => {
+    chicagoBars.create(req.body, (err, newBar)=> {
+        res.redirect('/bars')
+    })
+
+})
+
+// show bar route
+bars.get('/:id', (req, res) => {
+    chicagoBars.findById(req.params.id, (err, bar) => {
+        res.render('showbars.ejs', {
+            bar
+        })
+    })
+})
+
+
+// edit bar route
+bars.get('/edit/:id', (req, res) => {
+    chicagoBars.findById(req.params.id, (err, bar) => {
+        res.render('edit.ejs', {
+            bar
+        })
+    })
+})
+
+
+module.exports = bars
---
+++ models/bars.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const barSchema = new mongoose.Schema({
+    name:  { type: String, required: true },
+    description:  { type: String, required: true },
+    img: [String],
+    specials: {type: String, required: true },
+    location: {type: String, required: true },
+    barType: {type: String, required: true}
+    }
+);
+
+const bars = mongoose.model('Bars', barSchema);
+
+module.exports = bars;
